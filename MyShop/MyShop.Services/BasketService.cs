3ff83d6c61dd77077e8d27759c3fa01f7d4cbff7
alenrkn247 <alenrkn247@gmail.com>
2020-06-10 11:41:12 +0400

    Finished Basket Service

--- MyShop/MyShop.Services/BasketService.cs
+++ MyShop/MyShop.Services/BasketService.cs
@@ -1,5 +1,6 @@
 ﻿using MyShop.Core.Contracts;
 using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +10,7 @@ using System.Web;
 
 namespace MyShop.Services
 {
-    public class BasketService
+    public class BasketService : IBasketService
     {
         IRepository<Product> productContext;
         IRepository<Basket> basketContext;
@@ -97,6 +98,52 @@ namespace MyShop.Services
             {
                 basket.Basketitems.Remove(item);
                 basketContext.Commit();
+            }
+        }
+        public List<BasketitemViewModel> GetBasketItems(HttpContextBase httpContext)
+        {
+            Basket basket = GetBasket(httpContext, false);
+            if (basket != null)
+            {
+                var results = (from b in basket.Basketitems
+                               join p in productContext.Collection() on b.ProductId equals p.Id
+                               select new BasketitemViewModel()
+                               {
+                                   Id = b.Id,
+                                   Quantity = b.Quantity,
+                                   Productname = p.Name,
+                                   Image = p.Image,
+                                   Price = p.Price
+                               }
+                              ).ToList();
+
+                return results;
+            }
+            else
+            {
+                return new List<BasketitemViewModel>();
+            }
+        }
+        public BasketSummaryViewModel GetBackSummay(HttpContextBase httpContext)
+        {
+            Basket basket = GetBasket(httpContext, false);
+            BasketSummaryViewModel model = new BasketSummaryViewModel(0, 0);
+            if (basket != null)
+            {
+                //? gives the ability to store null as well
+                int? basketCount = (from item in basket.Basketitems
+                                    select item.Quantity).Sum();
+                decimal? basketTotal = (from item in basket.Basketitems
+                                        join p in productContext.Collection() on item.ProductId equals p.Id
+                                        select item.Quantity * p.Price).Sum();
+                model.BasketCount = basketCount ?? 0;   //?? is a conditional operator and returns the right handvalue if the variable assigned is null
+                model.BasketTotal = basketTotal ?? decimal.Zero;
+
+                return model;
+            }
+            else
+            {
+                return model;
             }
         }
 
